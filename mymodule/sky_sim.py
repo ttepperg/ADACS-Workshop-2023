f931e0bdbe6fc2b14a9668d24ef47db46d702938
Thor Tepper-Garcia <ttepperg@gmail.com>
2023-03-22 12:00:30 +1100

    Added multiprocessing

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,112 +1,91 @@
 #! /usr/bin/env python
-"""
-
-Author: Paul Hancock (2023)
-
-About: Create a synthetic sky catalogue where:
-
-- Stars should have randomized sky positions around the Andromeda galaxy
-- Positions should fall within 1 degree of the central location
-- Each star should have a unique ID
-- The star ID and position should be saved in a csv file to be analyzed by other programs
-
-"""
-
-import math
-import random
-import argparse
-import matplotlib.pyplot as plt
+# Demonstrate that we can simulate a catalogue of stars on the sky
 
 # Determine Andromeda location in ra/dec degrees
-# from wikipedia
-RA = '00:42:44.3'
-DEC = '41:16:09'
-NUM_STARS = 1_000  # _000 # underscores are stripped out by interpreter
+import math
+import numpy as np
+import multiprocessing
+import sys
+
+NSRC = 1_000_000
 
 
-# def get_radec(ra: str, dec: str) -> (float, float):  # using typing
 def get_radec():
-    """
-    Calculate the right-ascension and declination in degrees
-    _ra,_dec: 2-tuple with ra and dec in degrees
-    """
-    d, m, s = DEC.split(':')
-    _dec = int(d) + int(m) / 60 + float(s) / 3600
-    h, m, s = RA.split(':')
-    _ra = 15 * (int(h) + int(m) / 60 + float(s) / 3600)
-    _ra = _ra / math.cos(_dec * math.pi / 180)
-    return _ra, _dec
+    # from wikipedia
+    ra = '00:42:44.3'
+    dec = '41:16:09'
+
+    d, m, s = dec.split(':')
+    dec = int(d) + int(m) / 60 + float(s) / 3600
+
+    h, m, s = ra.split(':')
+    ra = 15 * (int(h) + int(m) / 60 + float(s) / 3600)
+    ra = ra / math.cos(dec * math.pi / 180)
+    return ra, dec
 
 
-def make_stars(num, ra, dec):
+def make_stars(args):
     """
-    Create an ensemble of stars with random coordinates one degree around (ra,dec)
-    num: number of stars
-    ra, dec: sky coordinates in degree
-    ras, decs: two lists with ra and dec values for each star
     """
-    ras = []
-    decs = []
-    for dummy in range(num):
-        ras.append(ra + random.uniform(-1, 1))
-        decs.append(dec + random.uniform(-1, 1))
-    return ras, decs
+    # unpack the arguments
+    ra, dec, nsrc = args
+    # create an empy array for our results
+    radec = np.empty((2, nsrc))
+
+    # make nsrc stars within 1 degree of ra/dec
+    radec[0, :] = np.random.uniform(ra - 1, ra + 1, size=nsrc)
+    radec[1, :] = np.random.uniform(dec - 1, dec + 1, size=nsrc)
+
+    # return our results
+    return radec
 
 
-def skysim_parser():
-    """
-    Configure the argparse for skysim
+def make_stars_parallel(ra, dec, nsrc=NSRC, cores=None):
+    # By default use all available cores
+    if cores is None:
+        cores = multiprocessing.cpu_count()
 
-    Returns
-    -------
-    parser : argparse.ArgumentParser
-        The parser for skysim.
-    """
-    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
-    parser.add_argument('--ra', dest='ra', type=float, default=None,
-                        help="Central ra (degrees) for the simulation location")
-    parser.add_argument('--dec', dest='dec', type=float, default=None,
-                        help="Central dec (degrees) for the simulation location")
-    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
-                        help='destination for the output catalog')
-    return parser
+    # 20 jobs each doing 1/20th of the sources
+    group_size = nsrc // 20
+    args = [(ra, dec, group_size) for _ in range(20)]
 
+    # start a new process for each task, hopefully to reduce residual
+    # memory use
+    ctx = multiprocessing.get_context()
+    pool = ctx.Pool(processes=cores, maxtasksperchild=1)
 
-def main():
-    """
-    Run the script
-    :return: None
-    """
-    # convert sky coordinates to decimal degrees
-    # ra, dec = get_radec(RA, DEC)
-
-    parser = skysim_parser()
-    options = parser.parse_args()
-    # if ra/dec are not supplied the use a default value
-    if None in [options.ra, options.dec]:
-        ra, dec = get_radec()
+    try:
+        # call make_posisions(a) for each a in args
+        results = pool.map(make_stars, args, chunksize=1)
+    except KeyboardInterrupt:
+        # stop all the processes if the user calls the kbd interrupt
+        print("Caught kbd interrupt")
+        pool.close()
+        sys.exit(1)
     else:
-        ra = options.ra
-        dec = options.dec
+        # join the pool means wait until there are results
+        pool.close()
+        pool.join()
 
-    # generate ensemble of stars within 1 degree of Andromeda
-    ras, decs = make_stars(NUM_STARS, ra, dec)
+        # crete an empty array to hold our results
+        radec = np.empty((2, nsrc), dtype=np.float64)
 
+        # iterate over the results (a list of whatever was returned from make_stars)
+        for i, r in enumerate(results):
+            # store the returned results in the right place in our array
+            start = i * group_size
+            end = start + group_size
+            radec[:, start:end] = r
+
+    return radec
+
+
+if __name__ == "__main__":
+    ra, dec = get_radec()
+    pos = make_stars_parallel(ra, dec, NSRC, 2)
     # now write these to a csv file for use by my other program
     out_dir = '/Users/tepper/tutorials/ADACS-Workshop-2023/project/data/'
-    out_file = options.out
-    f_name = open(f'{out_dir}/{out_file}', 'w', encoding='ascii')
-    print("id,ra,dec", file=f_name)
-    for i in range(NUM_STARS):
-        print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f_name)
-    f_name.close()
-
-    # visualise
-    plt.scatter(ras[:], decs[:], marker='.', s=2)
-    plt.xlabel('RA (deg)')
-    plt.ylabel('Dec (deg)')
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
+    out_file = f'{out_dir}/catalog.csv'
+    with open(out_file, 'w') as f:
+        print("id,ra,dec", file=f)
+        np.savetxt(f, np.column_stack((np.arange(NSRC), pos[0, :].T, pos[1, :].T)), fmt='%07d, %12f, > %12f')
